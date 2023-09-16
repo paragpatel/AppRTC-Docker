a20eba4f937ab88dc5495c45252e52a644084b38
Parag Patel <parag@techinnovative.net>
2023-09-16 17:09:26 +0530

    Improved docker file - Directly use letsencrypt generated ssl key/cert

--- apprtc-server/docker-run.sh
+++ apprtc-server/docker-run.sh
@@ -4,6 +4,13 @@ ip=`hostname -i`
 if [ "$1" != "" ] ; then
 	ip=$1
 fi
-
+ip=apprtc.privatesmsbox.com
+docker stop webrtc
+sleep 1
+docker rm webrtc
+sleep 1
 #docker run --rm --net=host -e PUBLIC_IP=`hostname -i` -it webrtc
-docker run --rm --net=host -e PUBLIC_IP=$ip -it webrtc
+#for linux
+docker run --name webrtc -d --rm --net=host -e PUBLIC_IP=$ip  -v ${PWD}/webrtc_avconf:/webrtc_avconf -it webrtc
+#for mac
+#docker run --rm -d --name webrtc -e PUBLIC_IP=$ip -p 442:443 -p 8089:8089 -p 3478:3478 -p 3478:3478/udp -p 3033:3033 -p 59000-59100:59000-59100/udp -v ${PWD}/webrtc_avconf:/webrtc_avconf -it webrtc
--- apprtc-server/run.sh
+++ apprtc-server/run.sh
@@ -5,15 +5,15 @@ sed -i "s/SERVER_PUBLIC_IP/${PUBLIC_IP}/g" /apprtc/out/app_engine/constants.py
 
 # vsilva
 # Generate ssl certs for domain
-echo "Generating SSL certs for domain $PUBLIC_IP"
-domain=$PUBLIC_IP
-openssl req -subj "/CN=$domain/O=C1AS/C=US" -newkey rsa:2048 -sha256 -nodes -keyout $domain-key.pem -out $domain.csr -outform PEM
-openssl req -in $domain.csr -text -noout -verify
-openssl x509 -req -in $domain.csr -CA /cert/cert-pebble.pem -CAkey /cert/key-pebble.pem -CAcreateserial -text -out $domain.pem -days 1024 -sha256
+#echo "Generating SSL certs for domain $PUBLIC_IP"
+# domain=$PUBLIC_IP
+# openssl req -subj "/CN=$domain/O=C1AS/C=US" -newkey rsa:2048 -sha256 -nodes -keyout $domain-key.pem -out $domain.csr -outform PEM
+# openssl req -in $domain.csr -text -noout -verify
+# openssl x509 -req -in $domain.csr -CA /cert/cert-pebble.pem -CAkey /cert/key-pebble.pem -CAcreateserial -text -out $domain.pem -days 1024 -sha256
 
 # copy to proper place (collider WS server)
-mv $domain.pem /cert/cert.pem
-mv $domain-key.pem /cert/key.pem
+# mv $domain.pem /cert/cert.pem
+# mv $domain-key.pem /cert/key.pem
 
 # apache2 SSL (deprecated)
 #cp /cert/key.pem  /etc/ssl/private/ssl-cert-snakeoil.key
@@ -26,5 +26,10 @@ echo "************ STARTUP ******************"
 echo "* PUBLIC_IP=${PUBLIC_IP}"
 echo "* Open https://${PUBLIC_IP}"
 echo "***************************************"
-supervisord -c /apprtc_supervisord.conf
+mkdir /webrtc_avconf
+nodejs /apprtc/ice.js &>/webrtc_avconf/ice_server.log &
+/goWorkspace/bin/collidermain -port=8089 -tls=true --room-server=0.0.0.0 &> /webrtc_avconf/collider.log &
+dev_appserver.py /apprtc/out/app_engine --skip_sdk_update_check --enable_host_checking=False --host=0.0.0.0 --ssl_certificate_path=/cert/cert.pem --ssl_certificate_key_path=/cert/key.pem --specified_service_ports default:442 &> /webrtc_avconf/apprtc_rs.log &
+turnserver -v -L 0.0.0.0 -a -f -r apprtc -c /etc/turnserver.conf --no-tls --no-dtls
+#supervisord -c /apprtc_supervisord.conf
 
